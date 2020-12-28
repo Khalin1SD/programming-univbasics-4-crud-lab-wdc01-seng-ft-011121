5a102e5f61a1ce49fbc3c7c19e3cadaf392fc47d
calm-collection-3916 <khalinredd@gmail.com>
2020-12-28 20:32:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array_remove
+  array_remove_end = ["arrays!", "End"]
+  array_remove_end.pop "End"
   
 end
 
