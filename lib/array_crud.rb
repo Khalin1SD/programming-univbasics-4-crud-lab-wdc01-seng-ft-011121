def0e7823b681ebd6b55f26340a5d739f1bcb21d
calm-collection-3916 <khalinredd@gmail.com>
2020-12-28 21:21:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,6 +39,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
+  retrieve_first = Array.new(3)
+  retrieve_first[0] = "apple"
+  
   
 end
 
